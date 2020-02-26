db8afb21593b26ef88f915aaac02d9e90f5e344c
jinchenxiangdan <jinchenxiangdan@gmail.com>
2020-02-26 16:24:59 -0700

    tested python script
    temp just write info into log file
    TODO: add move steps

--- DuplicateFileChecker/duplicate_file_checker.py
+++ DuplicateFileChecker/duplicate_file_checker.py
@@ -1,22 +1,12 @@
 import hashlib
-
+import os
 from os import listdir
 from os.path import isfile, isdir, join
-import os
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#
+# Author: Shawn Jin
+#
+#
 
 
 
@@ -25,9 +15,11 @@ def get_check_sum(input_path):
     # default is md5, could be changed to sha256 as need
     return hashlib.md5(open(input_path, "rb").read()).hexdigest()
 
+
 # list files under input path 
 def list_files(input_path):
     return [f for f in listdir(input_path) if isfile(join(input_path, f))]
+
 
 # list folders under input path, it will show invisiable files as well
 def list_folders(input_path):
@@ -45,49 +37,45 @@ def check_duplicate(directory):
         file_path = os.getcwd() + "/" + directory + "/" +file
         # check if the file name is valid or not 
         if file.startswith(prefix):
-
             checksum = get_check_sum(file_path)
 
             if checksum in MY_HASH_TABLE:
                 if file in FILE_NAME_TABLE:
                     # move to same content and same name folder
                     f =open(LOG_FILE_PATH, 'a')
-                    f.write("Complete duplicate file: move " + file_path + " to \n" + DUPLICATE_ALL_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
+                    f.write("Complete duplicate file: move " + file_path + "\nto                            " +
+                        DUPLICATE_ALL_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
                     f.close()
                 else:
                     # move to folder : same contant but different name folder 
                     f =open(LOG_FILE_PATH, 'a')
-                    f.write("Same content: move " + file_path + " to \n" + DUPLICATE_CONTENT_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
+                    f.write("Same content: (with " + MY_HASH_TABLE[checksum] +")\n move " + file_path + "\n to   " +
+                        DUPLICATE_CONTENT_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
                     f.close()
             else:
                 if file in FILE_NAME_TABLE:
                     f =open(LOG_FILE_PATH, 'a')
-                    f.write("Same content: move " + file_path + " to \n" + DUPLICATE_FILENAME_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
+                    f.write("Same filename: move " + file_path + "\nto                  " +
+                        DUPLICATE_FILENAME_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
                     f.close()
                 else:
                     # add to hash map and move to a folder : final 
                     MY_HASH_TABLE[checksum] = file
                     FILE_NAME_TABLE[file]   = 1
+                    f =open(LOG_FILE_PATH, 'a')
+                    f.write("Valid File: move " + file_path + "\n to              " +
+                        TARGET_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
+                    f.close()
         else:
             # move to invlaid name folder 
             f =open(LOG_FILE_PATH, 'a')
-            f.write("Invalid file name: move " + file_path + " to \n" + INVALID_NAME_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
+            f.write("Invalid file name: move " + file_path + "\nto                      " +
+                INVALID_NAME_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, "") + "\n")
             f.close()
 
-            #print("move \n", file_path, " to \n", INVALID_NAME_FOLDER_PATH + file_path.replace(CURRENT_WORK_SPACE, ""))
     for folders in list_folders(directory):
         check_duplicate(directory + "/" + folders)
 
-# global variables 
-MY_HASH_TABLE                   = {}
-FILE_NAME_TABLE                 = {}
-CURRENT_WORK_SPACE              = os.getcwd()
-INVALID_NAME_FOLDER_PATH        = CURRENT_WORK_SPACE + "/" + "invalid_filename_files"
-DUPLICATE_FILENAME_FOLDER_PATH  = CURRENT_WORK_SPACE + "/" + "duplicate_name_files"
-DUPLICATE_CONTENT_FOLDER_PATH   = CURRENT_WORK_SPACE + "/" + "duplicate_content_files"
-DUPLICATE_ALL_FOLDER_PATH       = CURRENT_WORK_SPACE + "/" + "duplicate_all_files"
-TARGET_FOLDER_PATH              = CURRENT_WORK_SPACE + "/" + "final"
-LOG_FILE_PATH                   = CURRENT_WORK_SPACE + "/" + "duplicate_checker_log"
 
 # create 4 folders that would be store all files. 
 def create_invalid_folders():
@@ -119,6 +107,19 @@ def create_invalid_folders():
     except FileExistsError:
         print("Directory ", TARGET_FOLDER_PATH,  "already exists.")
 
+
+# global variables 
+MY_HASH_TABLE                   = {}
+FILE_NAME_TABLE                 = {}
+CURRENT_WORK_SPACE              = os.getcwd()
+INVALID_NAME_FOLDER_PATH        = CURRENT_WORK_SPACE + "/" + "invalid_filename_files"
+DUPLICATE_FILENAME_FOLDER_PATH  = CURRENT_WORK_SPACE + "/" + "duplicate_name_files"
+DUPLICATE_CONTENT_FOLDER_PATH   = CURRENT_WORK_SPACE + "/" + "duplicate_content_files"
+DUPLICATE_ALL_FOLDER_PATH       = CURRENT_WORK_SPACE + "/" + "duplicate_all_files"
+TARGET_FOLDER_PATH              = CURRENT_WORK_SPACE + "/" + "final"
+LOG_FILE_PATH                   = CURRENT_WORK_SPACE + "/" + "duplicate_checker_log"
+
+
 def main():
 
 
@@ -126,9 +127,8 @@ def main():
     current_path = os.getcwd()
     # create invlaid forlders to store invalid files 
     create_invalid_folders()
-
-
-
+    # clear the log file
+    open(LOG_FILE_PATH, "w").close()
 
 
     # test area 
@@ -142,9 +142,16 @@ def main():
 
 
     # list all dict 
+    f = open(LOG_FILE_PATH, "a")
     for key, value in MY_HASH_TABLE.items():
-        print(key, " --> ", value)
-
+        # print(key, " --> ", value)
+        f.write("\n" + key + " -- > " + value)
+    f.write("\n")
+    # list name
+    for key, value in FILE_NAME_TABLE.items():
+        # print(key, " ==> ", value)
+        f.write("\n" + key + " ==> " + str(value))
+    f.close()
 
 if __name__ == "__main__":
     main()
