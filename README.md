cf0b5294d1507953e0897588e2421f095a63f262
jinchenxiangdan <jinchenxiangdan@gmail.com>
2020-03-03 16:32:42 -0700

    final test done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Tools
 It has some scripts/programs to help handle small problems
+
+
+Until now, I have:
+
+- Duplicate File Checker 
--- multi_thread_test/multi_thread_test.py
+++ multi_thread_test/multi_thread_test.py
@@ -4,16 +4,12 @@ from multiprocessing import Queue, Process
 import threading
 
 
-
-
-
-
-def function1(MY_QUEUE):
-    global IS_INPUT_DONE,lock
+def function1():
+    global MY_QUEUE, IS_INPUT_DONE,lock
     for i in range(1, 51):
         print("Inserting ", i, "...")
         MY_QUEUE.put(i)
-        # time.sleep(0.1)
+        time.sleep(0.1)
     lock.acquire()
     print(lock.locked())
     IS_INPUT_DONE = True
@@ -21,34 +17,28 @@ def function1(MY_QUEUE):
     print(lock.locked())
     print("Value is: ",IS_INPUT_DONE)
 
-def function2(MY_QUEUE):
-    global IS_INPUT_DONE,lock
+
+def function2():
+    global MY_QUEUE, IS_INPUT_DONE,lock
     while not IS_INPUT_DONE:
         while not MY_QUEUE.empty():
             print(MY_QUEUE.get())
-        print("Lock is: ", IS_INPUT_DONE)
+        # print("IS INPUT DONE is: ", IS_INPUT_DONE)
+
         
-
     print("Done")
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
     lock = threading.Lock()
     IS_INPUT_DONE = False
+    MY_QUEUE = Queue()
 
     print("Multithread testing start...")
-    MY_QUEUE = Queue()
-    p1 = Process(target=function1, args=(MY_QUEUE,))
+
+    p1 = Process(target=function1)
     p1.start()
-    p2 = Process(target=function2, args=(MY_QUEUE,))
+    p2 = Process(target=function2)
     p2.start()
 
     p1.join()
