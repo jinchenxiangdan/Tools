047089d7f0a912bfe81e8db41a4b3369e97b5648
zhenxiangjin <jinchenxiangdan@gmail.com>
2020-03-04 13:07:30 -0700

    stage for pull

--- multi_thread_test/multi_thread_test.py
+++ multi_thread_test/multi_thread_test.py
@@ -4,51 +4,6 @@ from multiprocessing import Queue, Process
 import threading
 
 
-<<<<<<< HEAD
-
-
-
-
-def function1(MY_QUEUE, lock):
-    for i in range(1, 51):
-        MY_QUEUE.put(i)
-        time.sleep(0.1)
-        print("Inserting ", i, "...")
-    lock.acquire()
-    IS_INPUT_DONE = True
-    lock.release()
-
-def function2(MY_QUEUE, lock):
-    while not IS_INPUT_DONE:
-        while not MY_QUEUE.empty():
-            print(MY_QUEUE.get())
-        print("Lock is: ", IS_INPUT_DONE)
-        
-
-    print("Done")
-
-
-
-
-
-
-
-global IS_INPUT_DONE
-
-
-
-if __name__ == "__main__":
-    lock = threading.Lock()
-
-    IS_INPUT_DONE = False
-    print("Multithread testing start...")
-    MY_QUEUE = Queue()
-    p1 = Process(target=function1, args=(MY_QUEUE, lock,))
-    p1.start()
-    p2 = Process(target=function2, args=(MY_QUEUE, lock,))
-    p2.start()
-
-=======
 def function1():
     global MY_QUEUE, IS_INPUT_DONE,lock
     for i in range(1, 51):
@@ -89,4 +44,3 @@ if __name__ == "__main__":
     p1.join()
     p2.join()
 
->>>>>>> master
