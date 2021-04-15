6cf512ee076ebb64fab7a3b2c0d9b7dc2b076c10
Shawn Jin <jinchenxiangdan@gmail.com>
2021-04-15 14:07:26 -0700

    Create fake.js
    create fake user data

---
+++ fake_users_generator/fake.js
@@ -0,0 +1,28 @@
+const fetch = async() => {
+	try {
+		// some thing
+	} catch (error) {
+		console.log('Error on fetching', error)
+	}
+}
+
+
+
+
+const targetFile = "example_data.json";
+
+sjd = {
+    "id": {
+        "current_rank_score": 300
+    }
+}
+
+import fs from 'fs';
+var data = {
+};
+
+
+
+
+
+fs.writeFile(targetFile)
